d016bdc1de7d6a4ed255c485806cba83905ca9ef
Adam Neef <adneef@gmail.com>
2018-01-10 11:18:34 -0700

    Segueing complete.  About to move to scene view content.

--- Journey/ViewController.swift
+++ Journey/ViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  @IBAction func StartReadingCold(_ sender: UIButton) {
-  }
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
